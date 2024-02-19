b683564d69a482a187ad15b0c0bf6c2bd1d0a122
amanyih <amanuelyihunie27@gmail.com>
2024-02-19 16:10:58 +0300

    feat: add photo on product create

--- hawk-server/src/product/controller/product.controller.ts
+++ hawk-server/src/product/controller/product.controller.ts
@@ -6,17 +6,22 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFiles,
 } from '@nestjs/common';
 import { ProductService } from '../service/product.service';
 import { CreateProductDto } from '../dto/create-product.dto';
 import { UpdateProductDto } from '../dto/update-product.dto';
+import { FileFieldsInterceptor, FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
+  @UseInterceptors(FileInterceptor('photos'))
+  create(@UploadedFiles() files: Express.Multer.File[], @Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
