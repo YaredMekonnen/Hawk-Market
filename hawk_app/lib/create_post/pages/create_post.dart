5899a3c3875232d2254975ffdae23faec460ffb0
amanyih <amanuelyihunie27@gmail.com>
2024-03-06 13:19:53 +0300

    feat: added create post page with barrel file

---
+++ hawk_app/lib/create_post/pages/create_post.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CreatePostPage extends StatefulWidget {
+  
+  @override
+  State<CreatePostPage> createState() => _CreatePostPageState();
+}
+
+class _CreatePostPageState extends State<CreatePostPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign In'),
+      ),
+    );
+  }
+}
