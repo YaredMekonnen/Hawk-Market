a10a81a0f8d7b3b235886e5a331812c942be56a7
amanyih <amanuelyihunie27@gmail.com>
2024-03-11 17:14:44 +0300

    fix: changed some routes

--- hawk_app/lib/route/route.dart
+++ hawk_app/lib/route/route.dart
@@ -2,28 +2,26 @@ import "package:hawk_app/auth/pages/index.dart";
 import "package:hawk_app/chat/pages/index.dart";
 import "package:hawk_app/create_post/pages/create_post.dart";
 import "package:hawk_app/home/pages/index.dart";
+import "package:hawk_app/home/pages/item_list_page.dart";
 import "package:hawk_app/profile/pages/index.dart";
 
 import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
+import "package:hawk_app/theme/theme.dart";
 
 class AppRouter extends StatelessWidget {
 
 
-  late GoRouter _router = GoRouter(
-    routes: <RouteBase>[
-      StatefulShellRoute.indexedStack(
-        branches: <StatefulShellBranch>[
-          StatefulShellBranch(routes: [])
-        ]
-      )
-    ], 
-    initialLocation: "/");
-
+  GlobalKey<NavigatorState> parentKey = GlobalKey<NavigatorState>();
+  GlobalKey<NavigatorState> homeKey = GlobalKey<NavigatorState>();
+  GlobalKey<NavigatorState> profileKey = GlobalKey<NavigatorState>();
+  GlobalKey<NavigatorState> createPostKey = GlobalKey<NavigatorState>();
 
   @override
   Widget build(BuildContext context){
     return MaterialApp.router(
+      title: "Hawk",
+      theme: ThemeClass.lightTheme,
       routerConfig: GoRouter(
         initialLocation: "/",
         routes: <RouteBase>[
@@ -33,47 +31,64 @@ class AppRouter extends StatelessWidget {
             },
             branches: <StatefulShellBranch>[
               StatefulShellBranch(
+                navigatorKey: homeKey,
                 routes: [
                   GoRoute(
                     path: "/",
                     builder: (context, state) {
-                      return HomePage();
+                      return ItemListPage();
                     },
                     routes: <RouteBase>[
                       GoRoute(
-                        path: "/profile",
+                        path: "search",
+                        builder: (context, state) {
+                          return SearchPage();
+                        }
+                      ),
+                      GoRoute(
+                        path: "profile",
                         builder: (context, state) {
                           return SelfProfilePage();
                         }
                       ),
                       GoRoute(
-                        path: "/profile/:id",
+                        path: "profile/:id",
                         builder: (context, state) {
                           return OtherProfilePage();
                         }
                       ),
                       GoRoute(
-                        path: "/chat",
+                        path: "chat",
                         builder: (context, state) {
                           return ChatPage();
                         }
                       ),
                     ]
                   ),
+                ]
+              ),
+              StatefulShellBranch(
+                navigatorKey: profileKey,
+                routes: [
+                  GoRoute(
+                    path: "/profile",
+                    builder: (context, state) {
+                      return SelfProfilePage();
+                    }
+                  ),
+                ]
+              ),
+              StatefulShellBranch(
+                navigatorKey: createPostKey,
+                routes: [
                   GoRoute(
                     path: "/create-post",
                     builder: (context, state) {
                       return CreatePostPage();
                     }
                   ),
-                  GoRoute(
-                    path: "/chat",
-                    builder: (context, state) {
-                      return ChatPage();
-                    }
-                  ),
                 ]
-              )
+              ),
             ]
           )
         ],
