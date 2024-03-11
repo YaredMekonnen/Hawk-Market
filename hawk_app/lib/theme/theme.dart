b0080519b8beaf9cd609592c3c6d1ebe68fa1d2e
amanyih <amanuelyihunie27@gmail.com>
2024-03-11 17:14:13 +0300

    feat: added custom theme to the app

---
+++ hawk_app/lib/theme/theme.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ThemeClass {
+  static Color primarColor = const Color.fromRGBO(255, 205, 0, 100);
+  static Color secondaryColor = const Color.fromRGBO(255, 255, 255, 100);
+  static Color backgroundColor = const Color.fromRGBO(40, 40, 40, 100);
+  static Color secondaryColor2 = const Color.fromRGBO(91, 97, 110, 100);
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: const ColorScheme.dark().copyWith(
+      primary: primarColor,
+      secondary: secondaryColor,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        minimumSize: MaterialStateProperty.all(
+          const Size(double.infinity, 55)
+        ),
+        backgroundColor: MaterialStateProperty.resolveWith<Color>((states) => primarColor),
+        shape: MaterialStateProperty.resolveWith<OutlinedBorder>((states) {
+          return RoundedRectangleBorder(borderRadius: BorderRadius.circular(10));
+        }),
+        foregroundColor: MaterialStateProperty.all<Color>(secondaryColor)
+      )
+    ),
+    textTheme: TextTheme(
+        displayLarge: GoogleFonts.getFont(
+            'Outfit',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: secondaryColor
+        ),
+        displayMedium: GoogleFonts.getFont(
+            'Outfit',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: secondaryColor
+        ),
+        displaySmall: GoogleFonts.getFont(
+            'Outfit',
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: secondaryColor
+        ),
+        titleLarge: GoogleFonts.getFont(
+            'Outfit',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: secondaryColor
+        ),
+        titleMedium: GoogleFonts.getFont(
+            'Outfit',
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            color: secondaryColor
+        ),
+        titleSmall: GoogleFonts.getFont(
+            'Outfit',
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+            color: secondaryColor
+        ),
+        bodyLarge:GoogleFonts.getFont(
+            'Outfit',
+            fontSize: 16,
+            color: secondaryColor
+        ),
+        bodyMedium: GoogleFonts.getFont(
+            'Outfit',
+            fontSize: 14,
+            color: secondaryColor
+        ),
+        bodySmall: GoogleFonts.getFont(
+          'Outfit',
+          fontSize: 12,
+          color: secondaryColor.withOpacity(0.4)
+        ),
+        
+      ),
+      iconTheme: IconThemeData(
+        color: Colors.orange,
+        size: 24,
+      ),
+  );
+}
+
+ThemeClass _themeClass = ThemeClass();
