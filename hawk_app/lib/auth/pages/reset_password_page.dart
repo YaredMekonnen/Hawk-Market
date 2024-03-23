0aea6375c5d460a9e87ab0d22e7a6b58c6117ef6
amanyih <amanuelyihunie27@gmail.com>
2024-03-23 21:18:41 +0300

    fix: ui changes on reset password page

--- hawk_app/lib/auth/pages/reset_password_page.dart
+++ hawk_app/lib/auth/pages/reset_password_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hawk_app/commons/textfield.dart';
+import "package:responsive_sizer/responsive_sizer.dart";
 
 class ResetPasswordPage extends StatefulWidget {
   
@@ -11,7 +14,66 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sign In'),
+        backgroundColor: Colors.transparent,
+        surfaceTintColor: Colors.transparent,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: 100.w,
+          height: 85.h,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('assets/reset-password.png'),
+                width: 90.w,
+                fit: BoxFit.contain,
+              ),
+              Container(
+                width: 90.w,
+                child: Column(
+                  children: [
+                    Text(
+                      'Reset Password',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    SizedBox(height: 4.w,),
+                    Text(
+                      'Please enter a secure password that you can easily remember.',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CustomeTextField(hintText: "Old Password"),
+                  SizedBox(
+                    height: 4.w,
+                  ),
+                  CustomeTextField(hintText: "New Password")
+                ],
+              ),
+          
+              ElevatedButton(
+                onPressed: ()=>{}, 
+                child: Text(
+                  'Set Password',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.background
+                  ),
+                ),
+                style: ButtonStyle(
+
+                  minimumSize: MaterialStateProperty.resolveWith((states) => Size(90.w, 13.w)),
+                ),
+              ),
+            ]
+          ),
+        ),
       ),
     );
   }
