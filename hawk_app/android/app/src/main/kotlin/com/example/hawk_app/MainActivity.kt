0fab08bf9fd8b993e39618a82700e0fb50e9b520
amanyih <amanuelyihunie27@gmail.com>
2024-07-01 00:14:39 +0300

    chore: version mismatch with flutter_stripe

--- hawk_app/android/app/src/main/kotlin/com/example/hawk_app/MainActivity.kt
+++ hawk_app/android/app/src/main/kotlin/com/example/hawk_app/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.hawk_app
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
